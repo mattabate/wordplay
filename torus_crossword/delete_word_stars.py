7b7bf4ba358ba4e6d9f98534c9d8607040c2e020
Matt Abate <mabate13@gmail.com>
2024-08-30 02:31:33 -0400

    saving with new solutions

--- torus_crossword/delete_word_stars.py
+++ torus_crossword/delete_word_stars.py
@@ -6,13 +6,13 @@ def transpose(grid: list[str]) -> list[str]:
 
 
 file = "star_sols.json"
-file = "star_sols_flipped.json"
+# file = "star_sols_flipped.json"
 with open(file, "r") as f:
     star_sols = json.load(f)
 
-word = "DELARENTA"
+word = "SLATEBLUE"
 
-print(len(star_sols))
+initial_num = len(star_sols)
 new_star_sols = []
 
 for star in star_sols:
@@ -20,18 +20,19 @@ for star in star_sols:
     fails = False
     for line in star:
         if word in line:
-            print("found" + word)
+            print("found " + word)
             break
     else:
         for line in transpose(star):
             if word in line:
-                print("found" + word)
+                print("found " + word)
                 break
         else:
             new_star_sols.append(star)
 
 
-print(len(new_star_sols))
+final_num = len(new_star_sols)
+print("number removed ", initial_num - final_num)
 
-# with open(file, "w") as f:
-#     json.dump(new_star_sols, f, indent=2, ensure_ascii=False)
+with open(file, "w") as f:
+    json.dump(new_star_sols, f, indent=2, ensure_ascii=False)
