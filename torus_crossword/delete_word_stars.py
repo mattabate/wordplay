8599016b2962105679900b518653d922cb907602
Matt Abate <mabate13@gmail.com>
2024-08-31 13:54:14 -0400

    saving

--- torus_crossword/delete_word_stars.py
+++ torus_crossword/delete_word_stars.py
@@ -5,34 +5,32 @@ def transpose(grid: list[str]) -> list[str]:
     return ["".join(row) for row in zip(*grid)]
 
 
-file = "star_sols.json"
-# file = "star_sols_flipped.json"
-with open(file, "r") as f:
-    star_sols = json.load(f)
+for file in ["star_sols.json", "star_sols_flipped.json"]:
+    with open(file, "r") as f:
+        star_sols = json.load(f)
 
-word = "STONEAXES"
+    word = "HINDEMITH"
 
-initial_num = len(star_sols)
-new_star_sols = []
+    initial_num = len(star_sols)
+    new_star_sols = []
 
-for star in star_sols:
+    for star in star_sols:
 
-    fails = False
-    for line in star:
-        if word in line:
-            print("found " + word)
-            break
-    else:
-        for line in transpose(star):
+        fails = False
+        for line in star:
             if word in line:
                 print("found " + word)
                 break
         else:
-            new_star_sols.append(star)
+            for line in transpose(star):
+                if word in line:
+                    print("found " + word)
+                    break
+            else:
+                new_star_sols.append(star)
 
+    final_num = len(new_star_sols)
+    print("number removed ", initial_num - final_num)
 
-final_num = len(new_star_sols)
-print("number removed ", initial_num - final_num)
-
-with open(file, "w") as f:
-    json.dump(new_star_sols, f, indent=2, ensure_ascii=False)
+    with open(file, "w") as f:
+        json.dump(new_star_sols, f, indent=2, ensure_ascii=False)
