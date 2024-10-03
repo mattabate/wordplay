72d902e5b3e625ed581cd3e2af0736596801d504
Matt Abate <mabate13@gmail.com>
2024-10-02 21:20:31 -0400

    saving

--- torus_crossword/config.py
+++ torus_crossword/config.py
@@ -33,7 +33,7 @@ C_WALL = "█"
 IC_TYPE = "DA"  # da = flipped
 MAX_WAL = 42
 SEARCH_W_FLIPPED = True
-f_verbose = True
+f_verbose = False
 f_save_words_used = False
 f_save_bounds = [1, 40]
 SLEEP_DURATION = 120
@@ -80,7 +80,7 @@ GRID_TEMPLATE_FLIPPED = [
 # generate_initials.py config
 # searches for completed 10x12 initial conditions
 ###############################################
-STAR_SEARCH_W_FLIPPED = False
+STAR_SEARCH_W_FLIPPED = True
 STAR_SEARCH_VERBOSE = False
 
 # location of start of the star
