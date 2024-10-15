9874bbdd00b7bd0621ddcb3e62b5d50531fe04fc
Matt Abate <mabate13@gmail.com>
2024-10-15 02:41:09 -0400

    saving

--- torus_crossword/delete.py
+++ torus_crossword/delete.py
@@ -1,67 +1,25 @@
-import json
-import time
 import tqdm
-from config import C_WALL
 from torus.json import append_json, load_json
-from config import (
-    NEW_STARS_FOUND_JSON,
-    NEW_STARS_FOUND_FLIPPED_JSON,
-    NEW_STARS_CHECKED_JSON,
-    NEW_STARS_CHECKED_FLIPPED_JSON,
-    STARS_CHECKED_FLIPPED_JSON,
-    STAR_SEARCH_W_FLIPPED,
-    STAR_SEARCH_VERBOSE,
-    STARS_CHECKED_JSON,
-    STAR_TEMPLATE,
-    STAR_FLIPPED_TEMPLATE,
-)
+import json
 from lib import (
-    Direction,
-    Sqaure,
-    Word,
     WORDLIST_BY_LEN,
-    transpose,
-    replace_char_in_grid,
-    T_NORMAL,
-    T_GREEN,
-    T_PINK,
 )
 from torus.strings import get_prefix, get_suffix
 
-f_verbose = STAR_SEARCH_VERBOSE
-
-# across words length 9
-# down words length 8
-TEMPLATE = ["@@@@@@", "@@@@@@", "@@@@@@", "@@@@@@", "@@@@@@", "@@@@@@"]
-if not STAR_SEARCH_W_FLIPPED:
-    SOL_JSON = NEW_STARS_FOUND_JSON
-    CHE_JSON = NEW_STARS_CHECKED_JSON
-else:
-    SOL_JSON = NEW_STARS_FOUND_FLIPPED_JSON
-    CHE_JSON = NEW_STARS_CHECKED_FLIPPED_JSON
 
 # testing these here for now
-words_for_across = WORDLIST_BY_LEN[9]
+words_for_across = WORDLIST_BY_LEN[14]
 
-across_pref_set = list(set(get_prefix(word=w, len_pref=6) for w in words_for_across))
-across_suff_set = list(set(get_suffix(word=w, len_suff=6) for w in words_for_across))
-
-
-seen_previously = load_json(STARS_CHECKED_FLIPPED_JSON)
-
-prefixes_seen = list(set(get_prefix(word=w, len_pref=6) for w in seen_previously))
-
-word_to_add = []
-for s in tqdm.tqdm(prefixes_seen):
-    words_with_suffix = [
-        w for w in words_for_across if w.startswith(s)
-    ]  # all possible words in wordlist with that suffix
-
-    for x in words_with_suffix:
-        if x not in seen_previously:
-            break
-    else:
-        word_to_add.append(s)
-
-with open("ic_data/new_stars_checked_flipped.json", "w") as f:
-    json.dump(word_to_add, f, indent=4)
+words = set()
+for w in tqdm.tqdm(words_for_across):
+    w1 = w[::4]
+    w2 = w[1::4]
+    w3 = w[2::4]
+    w4 = w[3::4]
+    if (
+        w1 in WORDLIST_BY_LEN[len(w1)]
+        and w2 in WORDLIST_BY_LEN[len(w2)]
+        and w3 in WORDLIST_BY_LEN[len(w3)]
+        and w4 in WORDLIST_BY_LEN[len(w4)]
+    ):
+        print(w, w1, w2, w3, w4)
