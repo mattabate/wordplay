ba14a63ab97e7bf2ef4b04887510cbadba38cad8
Matt Abate <mabate13@gmail.com>
2024-11-02 16:58:52 -0400

    saving progress

--- torus_crossword/api_sort.py
+++ torus_crossword/api_sort.py
@@ -24,7 +24,7 @@ class Source(enum.Enum):
     good_words: int = 3
 
 
-source = Source.active_grids
+source = Source.in_consideration
 
 without_clues_only = False
 f_delete_active = True
--- torus_crossword/config.py
+++ torus_crossword/config.py
@@ -18,8 +18,8 @@ class Mode(enum.Enum):
 
 mode = Mode.DA  # da on personal
 f_verbose = True
-f_save_words_used = False
-f_save_bounds = [0, 3]
+f_save_words_used = True
+f_save_bounds = [0, 1]
 SLEEP_DURATION = -1
 
 
@@ -69,7 +69,7 @@ with open("liked_templates.json", "r") as f:
 
 # GRID_TEMPLATE_FLIPPED = liked_templates[
 #     6
-# ]  # 4 (1), 5 (2) was still going, after solutions but want to test 5
+# ]  # 4 (1), 5 (2), 6 (2) was still going, after solutions but want to test 5
 
 # GRID_TEMPLATE_FLIPPED = [
 #     "@@@█@@@@█@@@@@@",
