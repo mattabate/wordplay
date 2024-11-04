126fd22ef7521609735df1240a3d79937321adac
Matt Abate <mabate13@gmail.com>
2024-11-04 09:23:46 -0500

    saving more solutions

--- torus_crossword/config.py
+++ torus_crossword/config.py
@@ -20,7 +20,7 @@ mode = Mode.DA  # da on personal
 f_verbose = True
 f_save_words_used = True
 f_save_bounds = [0, 5]
-SLEEP_DURATION = 75
+SLEEP_DURATION = -1
 
 
 if mode == Mode.AD:
@@ -67,14 +67,13 @@ import json
 with open("liked_templates.json", "r") as f:
     liked_templates = json.load(f)
 
-GRID_TEMPLATE_FLIPPED = liked_templates[5]
+# GRID_TEMPLATE_FLIPPED = liked_templates[6]
 
 # 0, 1, 3 has failed
 # 2 got down to 1, no solutions found
 # 4 got down to 2, no solutions found
-
-# 4, 5 unkillable
-# 6 has solutions
+# 5 got down to 2, no solutions found
+# 6 got many solutions
 
 # GRID_TEMPLATE_FLIPPED = [
 #     "@@@█@@@@█@@@@@@",
