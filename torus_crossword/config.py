858ba91f1f7595b26978bf39c718885f81beef30
Matt Abate <mabate13@gmail.com>
2024-11-09 01:51:33 -0500

    saving before new sort

--- torus_crossword/config.py
+++ torus_crossword/config.py
@@ -18,9 +18,9 @@ class Mode(enum.Enum):
 
 mode = Mode.DA  # da on personal
 f_verbose = False
-f_save_words_used = False
+f_save_words_used = True
 f_save_bounds = [0, 1]
-SLEEP_DURATION = 300
+SLEEP_DURATION = 75
 
 
 if mode == Mode.AD:
